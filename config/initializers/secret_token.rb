89fef6ab2e3256a828dc6cde77098dd47908a9a0
TeamBIneka <gulylug@gmail.com>
2012-11-20 11:21:05 +0400

    Order of dishes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderOfDishes::Application.config.secret_token = '4d3f2df0ba4b430e9572fe7935d1549ba7d6e0c2d4bbb587270840b440f3238a4157a827d89d1418a8c0a5bdc2de17260cedeb5c9ac697dba888b609bfa62ea2'
